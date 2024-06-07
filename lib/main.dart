10be9c90b96e0899aa605755bc1d34b9e0e6b157
sajeel093 <sajeelhassan53@gmail.com>
2024-06-07 17:12:01 +0500

    longtimeNoSee

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flashchat/screens/welcome_screen.dart';
+import 'package:flashchat/screens/login_screen.dart';
+import 'package:flashchat/screens/registration_screen.dart';
+import 'package:flashchat/screens/chat_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData.dark().copyWith(
+          textTheme: TextTheme(
+            bodyText1: TextStyle(color: Colors.black54),
+          ),
+        ),
+        initialRoute: WelcomeScreen.id,
+        routes: {
+          WelcomeScreen.id: (context) => WelcomeScreen(),
+          "/login": (context) => LoginScreen(),
+          "/registration": (context) => RegistrationScreen(),
+          "/chat": (context) => ChatScreen(),
+        });
+  }
+}
